b8b1ca3896fb43fe786bb4106bffc4bf7dffbaea
cjfelice <monocularmedia@gmail.com>
2020-05-26 01:45:48 +0000

    adding improved assertion function

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅ Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("❌ Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(2, 2);
+assertEqual("Fun", "Fun");
+assertEqual(3, 5);

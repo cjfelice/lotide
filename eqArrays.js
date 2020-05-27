669cb4311e20101401448e4fb22915d05510f118
cjfelice <monocularmedia@gmail.com>
2020-05-27 04:07:26 +0000

    added eqArrays function

---
+++ eqArrays.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(firstArr, secondArr) {
+  let arrayTest = true;
+  for (let i = 0; i < firstArr.length; i++) {
+  if (firstArr[i] !== secondArr[i]) {
+  arrayTest = false;
+  }
+  }
+  return arrayTest;
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);

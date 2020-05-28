1553ab207bf5948f0dd004d28a6e74bc4df70dc5
cjfelice <monocularmedia@gmail.com>
2020-05-28 05:28:42 +0000

    added letterPositions.js

---
+++ letterPositions.js
@@ -0,0 +1,36 @@
+const eqArrays = function(firstArr, secondArr) {
+  let arrayTest = true;
+  if (firstArr.length !== secondArr.length) {
+    arrayTest = false;
+  }
+  for (let i = 0; i < firstArr.length; i++) {
+    if (firstArr[i] !== secondArr[i]) {
+      arrayTest = false;
+    }
+  }
+  return arrayTest;
+};
+
+const assertArraysEqual = function(firstArr, secondArr) {
+  if (eqArrays(firstArr, secondArr) === true) {
+    console.log(`✅ Assertion Passed: ${firstArr} === ${secondArr}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${firstArr} !== ${secondArr}`);
+  }
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== " ") {
+      if (results[sentence[i]]) {
+        results[sentence[i]].push(i);
+      } else {
+        results[sentence[i]] = [i];
+      }
+    }
+  }
+  return results;
+};
+
+assertArraysEqual(letterPositions("lighthouse in the house")['h'], [3, 5, 15, 18]);

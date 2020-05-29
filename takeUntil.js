8af6310a2facb0fd77ee7e5997137e3f66bad4f4
cjfelice <monocularmedia@gmail.com>
2020-05-29 04:20:21 +0000

    added takeUntil.js but still a bit buggy

---
+++ takeUntil.js
@@ -0,0 +1,38 @@
+const eqArrays = function(firstArr, secondArr) {
+  let arrayTest = true;
+  if (firstArr.length !== secondArr.length) {
+    arrayTest = false;
+  }
+  for (let i = 0; i < firstArr.length; i++) {
+    if (firstArr[i] !== secondArr[i]) {
+      arrayTest = false;
+    }
+  }
+  return arrayTest;
+};
+
+const assertArraysEqual = function(firstArr, secondArr) {
+  if (eqArrays(firstArr, secondArr) === true) {
+    console.log(`✅ Assertion Passed: ${firstArr} === ${secondArr}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${firstArr} !== ${secondArr}`);
+  }
+};
+
+const takeUntil = function(array, callback) {
+  let newArr = [];
+  for (const item of array) {
+    if (item != array.filter(callback)) {
+      newArr.push(item);
+    } else {
+      return newArr;
+    }
+  }
+  return newArr;
+};
+
+const data1 = [1, 2, 5, '7', 2, -1, 2, 4, 5];
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+
+assertArraysEqual(takeUntil(data1, x => x < 0), [ 1, 2, 5, 7, 2 ]);
+assertArraysEqual(takeUntil(data2, x => x === ','), [ 'I\'ve', 'been', 'to', 'Hollywood' ]);

e4b9b19189b394333aa6eaf9e75233b5e435b1fa
cjfelice <monocularmedia@gmail.com>
2020-06-02 16:54:56 +0000

    re-added console.log command for assertion functions

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -2,8 +2,10 @@ const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(firstArr, secondArr) {
   if (eqArrays(firstArr, secondArr) === true) {
+    console.log(`✅ Assertion Passed: ${firstArr} === ${secondArr}`);
     return `✅ Assertion Passed: ${firstArr} === ${secondArr}`;
   } else {
+    console.log(`❌ Assertion Failed: ${firstArr} !== ${secondArr}`);
     return `❌ Assertion Failed: ${firstArr} !== ${secondArr}`;
   }
 };
--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -3,8 +3,10 @@ const eqObjects = require('./eqObjects');
 const assertObjectsEqual = function(firstObj, secondObj) {
   const inspect = require('util').inspect;
   if (eqObjects(firstObj, secondObj) === true) {
+    console.log(`✅ Assertion Passed: ${inspect(firstObj)} === ${inspect(secondObj)}`);
     return `✅ Assertion Passed: ${inspect(firstObj)} === ${inspect(secondObj)}`;
   } else {
+    console.log(`❌ Assertion Failed: ${inspect(firstObj)} !== ${inspect(secondObj)}`);
     return `❌ Assertion Failed: ${inspect(firstObj)} !== ${inspect(secondObj)}`;
   }
 };
